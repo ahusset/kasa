e79c1b0ffbbb3a7c9d470731cb36551f463b0ed8
Alexandre Husset <alexandre@ezacae.com>
2024-06-20 08:24:59 +0200

    ajustement footer, logo clickable et texte noir en mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 /** @format */
 
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importation de Link pour la navigation
 import logo from '../../images/LOGO.svg'; // Importation du logo
 import Nav from '../Nav/Nav'; // Importation du composant de navigation
 
@@ -8,7 +10,9 @@ export default function Header() {
     return (
         <header className='headerWrap'>
             <figure className='headerWrap__fig'>
-                <img className='logo' src={logo} alt="logo de l'agence Kasa" /> {/* Affichage du logo */}
+                <Link to="/"> {/* Envelopper l'image du logo avec Link */}
+                    <img className='logo' src={logo} alt="logo de l'agence Kasa" /> {/* Affichage du logo */}
+                </Link>
             </figure>
             <Nav className='nav-header' /> {/* Affichage de la navigation */}
         </header>
